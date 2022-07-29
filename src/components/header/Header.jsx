8af5558285598e43d387a9687f4f175d8d1bc008
Yahia <yahiamohamed.benabbou@edu.uca.ma>
2022-07-29 10:20:02 +0100

    V1.0.1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,18 +5,21 @@ import ME from "../../assets/me.png"
 import HeaderSocials from './HeaderSocials.jsx'
 function Header() {
   return (
-    <header id='home'>
+    <header id="home">
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Benabbou Yahia Mohamed</h1>
-        <h5 className="text-light">Full stack Devlopper</h5>
+        <h1>Benabbou Mohamed Yahia</h1>
+        <h5 className="text-light">Web Devlopper - AWS Solutions Architect</h5>
         <Cta />
         <HeaderSocials />
         <div className="hero">
           <img src={ME} alt="" srcset="" />
         </div>
-        </div>
-        <a href="#contact" className="scroll__down "> scroll down </a>
+      </div>
+      <a href="#contact" className="scroll__down ">
+        {" "}
+        scroll down{" "}
+      </a>
     </header>
   );
 }
