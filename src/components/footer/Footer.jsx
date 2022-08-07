a7e41c3765c9f92aefa6383d8325052734109844
beniaXcode <yahiamohamed.benabbou@edu.uca.ma>
2022-08-07 16:49:48 +0100

    rm-portfolio

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,9 +25,9 @@ function Footer() {
         <li>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
+       {/*<li>
           <a href="#testemonials">Testemonials</a>
-        </li>
+        </li>*/}
         <li>
           <a href="#contact">Contact</a>
         </li>
